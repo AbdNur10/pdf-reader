140ee4d584e89b19416e43a1a834bebd0c4c1701
AbdNur10 <abdullahnur0000@gmail.com>
2024-08-19 08:17:20 -0700

    modified:   package-lock.json 	modified:   package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PdfViewer from "./pdfViewer";
 
-function App() {
+const App = () => {
+  const pdfUrl =
+    "https://firebasestorage.googleapis.com/v0/b/sqauredcircle.appspot.com/o/images%2F1723720533899?alt=media&token=2d71c138-35c7-4490-bae6-2bb3f6f47f34";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PDF Viewer</h1>
+      <PdfViewer pdfUrl={pdfUrl} />
     </div>
   );
-}
+};
 
 export default App;
